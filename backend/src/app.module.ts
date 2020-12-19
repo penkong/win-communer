e68facc8bfed4dcf8bb52eb3117dc39148d7f2b5
mazda <nazemi.works@gmail.com>
2020-12-19 07:44:49 +0000

    add docker file and prettier and connected to mysql with docker image , mysql 8 is so buggy with node , we use 5.7 and command is in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,35 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// ------------------------- Packages ------------------------------
+
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+// ------------------------ Local ----------------------------------
+
+import configuration from '../config/configuration'
+
+// -----------------------------------------------------------------
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    // docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=root -d mysql/mysql-server:5.7
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      // database: 'communer',
+      entities: [],
+      synchronize: true,
+      retryAttempts: 10
+    })
+  ],
+
+  controllers: [],
+  providers: []
 })
+
+// -----------------------------------------------------------------
 export class AppModule {}
